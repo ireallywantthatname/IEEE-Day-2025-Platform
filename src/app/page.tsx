e4c95a66a48ab61a82b2030ec633a979787ccd24
AchiniSubasinghe <achinimaheshika12@gmail.com>
2025-09-23 11:54:46 +0530

    feat:initial commit with basic funtionality

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Engine from "@/components/Engine";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Engine />
+    </div>
+  );
+}
