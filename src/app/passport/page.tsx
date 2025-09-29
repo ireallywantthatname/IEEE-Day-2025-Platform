d0320441ef3c1d042bf3fd563fdcbee5c747a803
ireallywantthatname <rladsilva@students.nsbm.ac.lk>
2025-09-29 18:21:44 +0530

    temp: linting errors

--- src/app/passport/page.tsx
+++ src/app/passport/page.tsx
@@ -1,19 +1,25 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import { UserType } from "@/types";
 
 const Home = () => {
-  const storedData = localStorage.getItem("userData");
-  if (storedData) {
-    const userData: UserType = JSON.parse(storedData);
-    return (
-      <div className="flex flex-col items-center justify-center h-screen gap-8">
-        <div className="font-bold">{userData.full_name}</div>
-        <div className="bg-white size-40"></div>
-        <div className="animate-bounce">{userData.email}</div>
-      </div>
-    );
-  }
+  const [userData, setUserData] = useState<UserType | null>(null);
+
+  useEffect(() => {
+    const storedData = localStorage.getItem("userData");
+    if (storedData) {
+      setUserData(JSON.parse(storedData));
+    }
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-8">
+      <div className="font-bold">{userData?.full_name}</div>
+      <div className="bg-white size-40"></div>
+      <div className="animate-bounce">{userData?.email}</div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Engine.tsx
+++ src/components/Engine.tsx
@@ -1,153 +1,19 @@
 "use client";
-import { useState, useEffect } from "react";
 
-type Answer = {
-  text: string;
-  isCorrect: boolean;
-};
-
-
-type Question = {
-  question: string;
-  answers: Answer[];
-};
-
-type QuizState = {
-  currentQuestion: number;
-  score: number;
-  showResults: boolean;
-  questions: Question[];
-  isLoading: boolean;
-};
+import { useRouter } from "next/navigation";
 
 const Engine = () => {
-
-  const [state, setState] = useState<QuizState>({
-    currentQuestion: 0,
-    score: 0,
-    showResults: false,
-    questions: [],
-    isLoading: true,
-  });
-
-
-  useEffect(() => {
-    const fetchQuestions = async () => {
-      try {
-        const response = await fetch(
-          "https://opentdb.com/api.php?amount=10&type=multiple"
-        );
-        const data = await response.json();
-        const questions = data.results.map((item: any) => {
-          const incorrectAnswers = item.incorrect_answers.map(
-            (answer: string) => ({
-              text: answer,
-              isCorrect: false,
-            })
-          );
-          const correctAnswer = {
-            text: item.correct_answer,
-            isCorrect: true,
-          };
-          return {
-            question: item.question,
-            answers: [...incorrectAnswers, correctAnswer].sort(
-              () => Math.random() - 0.5
-            ),
-          };
-        });
-        setState((prevState) => ({
-          ...prevState,
-          questions,
-          isLoading: false,
-        }));
-      } catch (error) {
-        console.error("Failed to fetch questions:", error);
-      }
-    };
-
-    fetchQuestions();
-  }, []);
-
-
-  const handleAnswerClick = (isCorrect: boolean): void => {
-    if (isCorrect) {
-      setState((prevState) => ({ ...prevState, score: prevState.score + 1 }));
-    }
-
-    const nextQuestion = state.currentQuestion + 1;
-    if (nextQuestion < state.questions.length) {
-      setState((prevState) => ({
-        ...prevState,
-        currentQuestion: nextQuestion,
-      }));
-    } else {
-      setState((prevState) => ({ ...prevState, showResults: true }));
-    }
-  };
-  const resetQuiz = (): void => {
-    setState({
-      currentQuestion: 0,
-      score: 0,
-      showResults: false,
-      questions: state.questions,
-      isLoading: false,
-    });
-  };
-
-  if (state.isLoading) {
-    return (
-      <div className="">
-        <p>Loading quiz questions, please wait...</p>
-      </div>
-    );
-  }
-
-  if (state.questions.length === 0) {
-    return <div>No questions available.</div>;
-  }
-
-  const currentQuestion = state.questions[state.currentQuestion];
-
+  const router = useRouter();
   return (
-    <div className="">
-      {state.showResults ? (
-        <div className="">
-          <h2 className="">Results</h2>
-          <p className="">
-            You scored {state.score} out of {state.questions.length}
-          </p>
-          <button onClick={resetQuiz} className="">
-            Try Again
-          </button>
-        </div>
-      ) : (
-        <div className="">
-          <h2 className="">
-            Question {state.currentQuestion + 1}/{state.questions.length}
-          </h2>
-          <p
-            className=""
-            dangerouslySetInnerHTML={{ __html: currentQuestion.question }}
-          />
-          <div className="">
-            {currentQuestion.answers.map((answer, index) => (
-              <button
-                key={index}
-                onClick={() => handleAnswerClick(answer.isCorrect)}
-                className="w-full"
-              >
-                {answer.text}
-              </button>
-            ))}
-          </div>
-          <div className="">
-            <span className="">Score: {state.score}</span>
-          </div>
-        </div>
-      )}
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div
+        className="bg-white text-black px-2 py-1 hover:scale-95 transition-transform duration-300"
+        onClick={() => router.push("/signin")}
+      >
+        Sign-In
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Engine
+export default Engine;
