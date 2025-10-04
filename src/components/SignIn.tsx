9c9ccb26eaca158d74c23734cf242bfb7ca0a4e7
ireallywantthatname <rladsilva@students.nsbm.ac.lk>
2025-10-04 13:27:11 +0530

    feat: handle user null case

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { SignInSchema, SignInType } from "@/types";
@@ -20,6 +21,9 @@ const fields = [
 ];
 
 const Home = () => {
+    const [isFetching, setIsFetching] = useState(false);
+    const [isUserNull, setIsUserNull] = useState(false);
+
     const router = useRouter();
 
     const {
@@ -38,18 +42,28 @@ const Home = () => {
     const onSubmit: SubmitHandler<SignInType> = async (data) => {
         console.log(data);
         try {
+            setIsFetching(true);
             const user = await readUser(data.email, data.password);
-            console.log(user);
-            localStorage.setItem("userData", JSON.stringify(user));
+            if (user == null) {
+                setIsUserNull(true);
+
+            }
+            else {
+                console.log(user);
+                localStorage.setItem("userData", JSON.stringify(user));
+                router.push("/passport");
+            }
         } catch (e) {
             console.log(e);
         }
-        router.push("/passport");
+        finally {
+            setIsFetching(false);
+        }
     };
 
     return (
         <div className="relative z-50 flex flex-col bg-white/5 backdrop-blur-lg p-10 md:px-24 md:py-12">
-            <div className="text-2xl md:text-4xl font-semibold text-center mb-10">Sign In</div>
+            <div className={`${isUserNull ? "text-red-500 text-lg" : "text-2xl md:text-4xl"} font-semibold text-center h-20`}>{isUserNull ? "Something went wrong" : "Sign In"}</div>
             <form onSubmit={handleSubmit(onSubmit)}>
                 {fields.map((field, index) => (
                     <div key={index} className="h-28 w-60">
@@ -67,9 +81,9 @@ const Home = () => {
                 <div className="flex justify-center">
                     <button
                         type="submit"
-                        className="bg-white text-black px-4 py-2 hover:scale-95 transition-transform duration-300"
+                        className={`bg-white text-black px-4 py-2 hover:scale-95 transition-transform duration-300 ${isFetching && "animate-pulse"}`}
                     >
-                        Sign In
+                        {isFetching ? ". . ." : "Sign In"}
                     </button>
                 </div>
             </form>
